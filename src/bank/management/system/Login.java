2ce3f95af597689ba0f1f2d5847dc7c255da2c0c
HimanshuHeda <harsh967896@gmail.com>
2024-12-27 21:00:56 +0530

    first commit

---
+++ src/bank/management/system/Login.java
@@ -0,0 +1,15 @@
+package bank.management.system;
+
+import javax.swing.*;
+
+public class Login extends JFrame {
+    
+    Login(){
+        setSize(800,480);
+        
+    }
+    
+    public static void main(String[] args) {
+        new Login();
+    }
+}
